1059b7afa01458b449fc5606a251b9158b7e1506
bnddiep <bach.nhu.diep@gmail.com>
2020-10-28 15:36:30 -0700

    added the controller methods index, create, update, and destroy

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,2 +1,40 @@
 class ApartmentsController < ApplicationController
+    def index
+        apartments = Apartment.ApartmentsController
+        render json: apartments
+    end
+    
+    def create
+        apartment = Apartment.create(apartment_params)
+        if apartment.valid?
+            render json: apartment
+        else
+            render json: apartment.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        apartment = Apartment.find(params[:id])
+        apartment.update(apartment_params)
+        if apartment.valid?
+            render json: apartment
+        else
+            render json: apartment.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        apartment = Apartment.find(params[:id])
+        if apartment.destroy
+            render json: apartment
+        else
+            render json: apartment.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def apartment_params
+        params.require(:apartment).permit(:street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets, :user_id)
+    end
+
 end
